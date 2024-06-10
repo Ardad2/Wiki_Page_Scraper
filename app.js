3d8c37e887a280a4c8a3c0009f522b55bbf600f0
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2024-06-11 04:03:36 +0530

    Trying to parse the headings.

--- app.js
+++ app.js
@@ -26,11 +26,23 @@ const request = https.request({
 
             //const contentTableItems = [...page.matchAll(/ <span class="vector-toc-numb">(.*?) <\/span>/gm)].map(match => match[1])
 
+
             const contentTableItems = [...page.matchAll(/<div class="vector-toc-text">(.*?)<\/div>/gm).map(match => match[1])]
 
            // const contentTableItems = [...page.matchAll(/ <a href="()">(.*?) <\/a>/gm)].map(match => match[1])
 
+            console.log("Content Table Items: ")
+
             console.log(contentTableItems);
+
+            const headings = [...page.matchAll(/<span class="mw-headline">(.*?)<\/span>/gm).map(match => match[1])]
+            console.log("All the headings: ");
+
+            console.log(headings);
+
+
+
+
         })
     }
 )
