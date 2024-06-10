6d9a74e34cd159e34ae1433bb838d73f97aa97c2
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2024-06-11 03:50:41 +0530

    Successfully able to parse something.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const request = https.request({
 
             //const contentTableItems = [...page.matchAll(/ <span class="vector-toc-numb">(.*?) <\/span>/gm)].map(match => match[1])
 
-            const contentTableItems = [...page.matchAll(/ <h1>(.*?) <\/h1>/gm)]
+            const contentTableItems = [...page.matchAll(/<div class="vector-toc-text">(.*?)<\/div>/gm).map(match => match[1])]
 
            // const contentTableItems = [...page.matchAll(/ <a href="()">(.*?) <\/a>/gm)].map(match => match[1])
 
